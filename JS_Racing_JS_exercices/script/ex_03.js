fbfd319812e9648cec112ce627a8e2fe6c052f47
Emre Sahin <emresahin@MacBook-Air-de-Emre.local>
2023-04-19 17:13:57 +0200

    Exos 3

--- JS_Racing_JS_exercices/script/ex_03.js
+++ JS_Racing_JS_exercices/script/ex_03.js
@@ -1,13 +1,14 @@
 window.addEventListener(("load"), () => {
     const div = document.querySelector('footer div');
     div.addEventListener('click', function () {
-        let text
-        let name = prompt("What is your name?")
-        if (name == NULL || name == "") {
-            prompt("What is your name?")
-        } else {
-            text = "Are you sure " + name + " is your name ?"
+        let name
+        while (typeof (name) == "undefined") {
+            name = prompt("What is your name ?");
+            if (name != null && name != "" && window.confirm("Are you sure " + name + " is your name ?")) {
+                div.innerText = "Hello " + name + " !"
+            } else {
+                name = undefined
+            }
         }
-        div.innerHTML = text
     })
 })
